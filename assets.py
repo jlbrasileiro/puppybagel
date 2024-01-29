d02ebad8e28a3acc40302385acaede5ca3bcd9c2
Láisa Camilly <laisaco1@al.insper.edu.br>
2024-01-29 10:50:13 -0300

    mudanças

--- assets.py
+++ assets.py
@@ -86,30 +86,3 @@ for i, imagem in enumerate(imagens_cachorros):
     pygame.image.save(imagem, f'cachorro_{i}.png')
 
 #--------------------------------------------------------------------------------
-
-def load_spritesheet(spritesheet, rows, columns):
-    '''
-    Recebe uma imagem de sprite sheet e retorna uma lista de imagens. 
-    É necessário definir quantos sprites estão presentes em cada linha e coluna.
-    Essa função assume que os sprites no sprite sheet possuem todos o mesmo tamanho.
-    Calcula a largura e altura de cada sprite.
-    sprite_width = spritesheet.get_width() // columns
-    sprite_height = spritesheet.get_height() // rows
-    '''
-    
-    # Percorre todos os sprites adicionando em uma lista.
-    sprites = []
-    for row in range(rows):
-        for column in range(columns):
-            # Calcula posição do sprite atual
-            x = column * sprite_width
-            y = row * sprite_height
-            # Define o retângulo que contém o sprite atual
-            dest_rect = pygame.Rect(x, y, sprite_width, sprite_height)
-
-            # Cria uma imagem vazia do tamanho do sprite
-            image = pygame.Surface((sprite_width, sprite_height), pygame.SRCALPHA)
-            # Copia o sprite atual (do spritesheet) na imagem
-            image.blit(spritesheet, (0, 0), dest_rect)
-            sprites.append(image)
-    return sprites

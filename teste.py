70c075897de330af3cfec72aae5ac128c64154fc
Láisa Camilly <laisaco1@al.insper.edu.br>
2024-01-29 14:37:54 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,45 +1,36 @@
 import pygame
+import sys
 
-#cachorro imagem
-cachorros = pygame.image.load('assets\\img\\PuppyOrBagel.jpg')
+# Inicialização do Pygame
+pygame.init()
 
-colunas = 4
-linhas = 4
-#dados
-largura_foto = cachorros.get_width() // colunas
-altura_foto = cachorros.get_height() // linhas
+# Configurações da tela
+largura_tela = 900
+altura_tela = 700
+tela = pygame.display.set_mode((largura_tela, altura_tela))
+pygame.display.set_caption("Puppy or Bagel")
 
-#Função para cortar as imagens
-def carrega_imagens(cachorros, linhas, colunas):
-    # Percorre todos os sprites adicionando em uma lista.
-    imagens = []
-    for linha in range(linhas):
-        for coluna in range(colunas):
-            # Calcula posição do sprite atual
-            x = coluna * largura_foto
-            y = linha * altura_foto
-            # Define o retângulo que contém o sprite atual
-            dest_rect = pygame.Rect(x, y, largura_foto, altura_foto)
+# Carregar imagem
+imagem = pygame.image.load('assets\\img\\cachorro_1.png')  
 
-            # Cria uma imagem vazia do tamanho do sprite
-            image = pygame.Surface((largura_foto, altura_foto), pygame.SRCALPHA)
+# Obter retângulo da imagem
+retangulo_imagem = imagem.get_rect()
 
-            # Copia o sprite atual (do spritesheet) na imagem
-            image.blit(cachorros, (0, 0), dest_rect)
-            imagens.append(image)
+# Loop principal
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
+    # Limpar a tela
+    tela.fill((0, 0,0))  # Preencher a tela com branco
 
-    return imagens
+    # Desenhar a imagem na tela usando blit
+    tela.blit(imagem, retangulo_imagem)
 
-#essa lista imagens possui esses indices cachorros: [0, 2, 5, 7, 8, 10, 13, 15]
-#e esses indices donuts: [1, 3, 4, 6, 9, 11, 12, 14]
-
-# Chamada da função
-imagens_cachorros = carrega_imagens(cachorros, linhas, colunas)
-
-# Exemplo de como utilizar as imagens carregadas
-for i, imagem in enumerate(imagens_cachorros):
-    pygame.image.save(imagem, f'cachorro_{i}.png')
+    # Atualizar a tela
+    pygame.display.flip()
 
 
 

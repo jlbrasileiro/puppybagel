df204a051d8ac173db1324c3f612ac5d1141865f
Láisa Camilly <laisaco1@al.insper.edu.br>
2024-01-29 10:45:50 -0300

    removi as imagens, pq passei pro album

--- game_screen.py
+++ game_screen.py
@@ -25,6 +25,9 @@ def game_screen(window):
         # ----- Gera saídas
         window.fill(BLACK)  # Preenche com a cor branca
 
+        #carrega imagens
+        
         pygame.display.update()  # Mostra o novo frame para o jogador
+
 
     return state
